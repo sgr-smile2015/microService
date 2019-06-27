cb7629945fc371c2a6dfb4b4eda25997b92b1bef
sgr <sgrqihai997@gmail.com>
2019-06-27 18:11:37 +0800

    db init

--- config.py
+++ config.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 # Copyright © sgr
 # CreateTime: 2019-06-27 15:32:00
+import os
 
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevConfig(BaseConfig):
     DEBUG = True
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL')
 
 
 class ProConfig(BaseConfig):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
